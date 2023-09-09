8ece8c1aa39b6086d1b39198a7df9d30fad3e43b
Mikael Valonen <102473112+MikaelValonen@users.noreply.github.com>
2023-09-09 18:48:34 +0300

    Github Reistailee, niin joudun palauttamaan kaiken uudestaan

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, } from 'react-native';
+  
 export default function App() {
+  const [ostos, setOstos] = useState('');
+  const [data, setData] = useState([]);
+
+  const buttonAdd = () => {
+    if(ostos == ''){
+
+    }
+    else {
+      setData([...data, { key: ostos }]);
+      setOstos('');}
+    }
+  const buttonClear = () => {
+      setData([]);
+  }
   return (
     <View style={styles.container}>
-      <Text>Jos näkyy, Muutokset Toimivat!</Text>
       <StatusBar style="auto" />
+      <TextInput style={styles.input} onChangeText={ostos => setOstos(ostos)} value={ostos.toString()}/>
+      <View style={styles.miniContainer}>
+      <Button title='Add' style={styles.button} onPress={() => buttonAdd()}>Add</Button>
+      <Button title='Clear' style={styles.button} onPress={() => buttonClear()}>Clear</Button>
+      </View>
+      <View style={styles.container}>
+      <Text>Shopping list</Text>
+      <FlatList data={data} renderItem={({item}) => <Text>{item.key}</Text>} keyExtractor={(item, index) => index.toString()} />
+      </View>
     </View>
   );
 }
@@ -13,8 +36,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    margin: 25,
+    padding: 5,
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  miniContainer: {
+    width: 150,
+    margin: 5,
+    padding: 5,
+    flexDirection: 'row',
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  button: {
+    width: 150,
+    color: 'blue',
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
